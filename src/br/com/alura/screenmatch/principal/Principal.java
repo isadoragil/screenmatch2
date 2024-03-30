c5a92cd4b0d62b8eb4bf09d6e38a65bf81e56991
Isadora Gil <105384553+isadoragil@users.noreply.github.com>
2024-03-30 17:07:31 -0300

    1

--- src/br/com/alura/screenmatch/principal/Principal.java
+++ src/br/com/alura/screenmatch/principal/Principal.java
@@ -1,3 +1,5 @@
+package br.com.alura.screenmatch.principal;
+
 import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
 import br.com.alura.screenmatch.modelos.Episodio;
@@ -8,44 +10,38 @@ import java.util.ArrayList;
 
 public class Principal {
     public static void main(String[] args) {
-        Filme meuFilme = new Filme();
-        meuFilme.setNome("O poderoso chefão");
-        meuFilme.setAnoDeLancamento(1970);
-        meuFilme.setDuracaoEmMinutos(180);
-        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
+        Filme filme1 = new Filme("O poderoso chefão", 1970);
+        filme1.setDuracaoEmMinutos(180);
+        System.out.println("Duração do filme: " + filme1.getDuracaoEmMinutos());
 
-        meuFilme.exibeFichaTecnica();
-        meuFilme.avalia(8);
-        meuFilme.avalia(5);
-        meuFilme.avalia(10);
-        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
-        System.out.println(meuFilme.pegaMedia());
-        //meuFilme.somaDasAvaliacoes = 10;
-        //meuFilme.totalDeAvaliacoes = 1;
-        //System.out.println(meuFilme.pegaMedia());
+        filme1.exibeFichaTecnica();
+        filme1.avalia(8);
+        filme1.avalia(5);
+        filme1.avalia(10);
+        System.out.println("Total de avaliações: " + filme1.getTotalDeAvaliacoes());
+        System.out.println(filme1.pegaMedia());
+        //filme1.somaDasAvaliacoes = 10;
+        //filme1.totalDeAvaliacoes = 1;
+        //System.out.println(filme1.pegaMedia());
 
-        Serie lost = new Serie();
-        lost.setNome("Lost");
-        lost.setAnoDeLancamento(2000);
+        Serie lost = new Serie("Lost", 2000);
         lost.exibeFichaTecnica();
         lost.setTemporadas(10);
         lost.setEpisodiosPorTemporada(10);
         lost.setMinutosPorEpisodio(50);
         System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
 
-        Filme outroFilme = new Filme();
-        outroFilme.setNome("Avatar");
-        outroFilme.setAnoDeLancamento(2023);
-        outroFilme.setDuracaoEmMinutos(200);
+        Filme filme2 = new Filme("Avatar", 2023);
+        filme2.setDuracaoEmMinutos(200);
 
         CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
-        calculadora.inclui(meuFilme);
-        calculadora.inclui(outroFilme);
+        calculadora.inclui(filme1);
+        calculadora.inclui(filme2);
         calculadora.inclui(lost);
         System.out.println(calculadora.getTempoTotal());
 
         FiltroRecomendacao filtro = new FiltroRecomendacao();
-        filtro.filtra(meuFilme);
+        filtro.filtra(filme1);
 
         Episodio episodio = new Episodio();
         episodio.setNumero(1);
@@ -53,15 +49,13 @@ public class Principal {
         episodio.setTotalVisualizacoes(300);
         filtro.filtra(episodio);
 
-        var filmeDaIsa = new Filme();
-        filmeDaIsa.setNome("Crepúsculo");
-        filmeDaIsa.setAnoDeLancamento(2008);
-        filmeDaIsa.setDuracaoEmMinutos(200);
+        var filme3 = new Filme("Crepúsculo", 2008);
+        filme3.setDuracaoEmMinutos(200);
 
         ArrayList<Filme> listaDeFilmes = new ArrayList<>();
-        listaDeFilmes.add(filmeDaIsa);
-        listaDeFilmes.add(meuFilme);
-        listaDeFilmes.add(outroFilme);
+        listaDeFilmes.add(filme3);
+        listaDeFilmes.add(filme1);
+        listaDeFilmes.add(filme2);
         System.out.println("Tamanho da lista: " + listaDeFilmes.size());
         System.out.println("Primeiro filme: " + listaDeFilmes.get(0).getNome());
         System.out.println(listaDeFilmes.get(0));
