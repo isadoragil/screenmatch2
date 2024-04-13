84fe9aeaf1fbdc48428f5b72036aa014104a32e0
Isadora Gil <105384553+isadoragil@users.noreply.github.com>
2024-04-13 14:56:38 -0300

    Update PrincipalComListas.java

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -3,6 +3,7 @@ package br.com.alura.screenmatch.principal;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
+<<<<<<< Updated upstream
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -30,5 +31,37 @@ public class PrincipalComListas {
         System.out.println("Ordenando por ano:");
         System.out.println(lista);
     }
+=======
+>>>>>>> Stashed changes
 
+import java.util.ArrayList;
+
+public class PrincipalComListas {
+    public static void main(String[] args) {
+        Filme filme1 = new Filme("O poderoso chefão", 1970);
+        filme1.avalia(10);
+        Filme filme2 = new Filme("Avatar", 2023);
+        filme2.avalia(7);
+        var filme3 = new Filme("Crepúsculo", 2008);
+        filme3.avalia(9);
+        Serie lost = new Serie("Lost", 2000);
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(filme1);
+        lista.add(filme2);
+        lista.add(filme3);
+        lista.add(lost);
+
+        //Filme f1 = filme1;  aponta para a referência do filme1 e não o objeto em si
+
+        for (Titulo item: lista) {
+            System.out.println(item);
+            if (item instanceof Filme filme && filme.getClassificacao() > 2) {
+                //Filme filme = (Filme) item;
+                System.out.println("Classificação: " + filme.getClassificacao());
+            }
+        }
+
+        //lista.forEach(list -> System.out.println(list));
+    }
 }
