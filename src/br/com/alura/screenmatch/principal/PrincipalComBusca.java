8a0e00c7f70cc1691fe57f144bafaac1f3f905ab
Isadora Gil de Souza <105384553+isadoragil@users.noreply.github.com>
2024-04-16 17:14:49 -0300

    6

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -5,14 +5,22 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um filme para busca:");
+        var busca = sc.nextLine();
+        busca = busca.replaceAll(" ", "+");
+
+        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=4c19ce34";
         //HttpRequest
         //HttpResponse
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?t=matrix&apikey=4c19ce34"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
